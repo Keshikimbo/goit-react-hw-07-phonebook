335f0f0d55a5e06a01f03f685dcf9edc83359659
Ivan Onofriichuk <ivanonofrijcuk@gmail.com>
2023-03-02 22:37:46 +0200

    add redux async

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,30 @@
+import { AiOutlineClose } from 'react-icons/ai';
+import PropTypes from 'prop-types';
+
+import { useDispatch } from 'react-redux';
+import { deleteContact } from 'redux/operations';
+
+import { CloseButton, Icon } from './Contact.styles';
+
+export default function Contact({ contact }) {
+  const dispatch = useDispatch();
+
+  const { name, phone, id } = contact;
+  return (
+    <>
+      <Icon />
+      {name}: {phone}
+      <CloseButton type="button" onClick={() => dispatch(deleteContact(id))}>
+        <AiOutlineClose size={18} />
+      </CloseButton>
+    </>
+  );
+}
+
+Contact.propTypes = {
+  contact: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    phone: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+  }).isRequired,
+};
